3cf5b24ac08b7c6c904551a0b90f909ae1791d28
vitali.b <vitali.b.dev@gmail.com>
2018-04-25 22:58:04 +0300

    fix

--- instructions.lua
+++ instructions.lua
@@ -1,19 +1,19 @@
 local me = {}
 
 local function oswget(data)
-    os.execute("wget -f https://raw.githubusercontent.com/OrlaithMichael/-experimental/master/"..data)
+    return os.execute("wget -f https://raw.githubusercontent.com/OrlaithMichael/-experimental/master/"..data)
 end
 
 local function osmkdir(data)
-    os.execute("mkdir data")
+    return os.execute("mkdir data")
 end
 
 local function osmv(data1, data2)
-    os.execute("mv data1, data2")
+    return os.execute("mv data1, data2")
 end
 
 local function osrm(data)
-    os.execute("rm -v data")
+    return os.execute("rm -v data")
 end
 
 function me.up()
