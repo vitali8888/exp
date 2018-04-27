155a42f8755bb2e79124ba3a3857580446e8458d
vitali.b <vitali.b.dev@gmail.com>
2018-04-27 15:38:13 +0300

    fix

--- robot/eventController.lua
+++ robot/eventController.lua
@@ -1,44 +1,59 @@
-local me = {}
+local obj = {}
 
-local events = {}
+    local events = {}
 
-function me.add(name, signal, handler)
+    obj.__index = obj
 
-    events[name] = {}
-    events[name].signal = signal
-    events[name].handler = handler
+    function obj.new(...)
+        if obj._instance then
+            return obj._instance
+        end
 
-    local eventHandler = require ("eventHandler/"..handler)
+        local instance = setmetatable({}, obj)
+        if instance.ctor then
+            instance.ctor(...)
+        end
 
-    local event = require ("event")
-    event.listen(signal, "eventHandler.do")
-
-    return eventHandler
-
-end
-
-function me.drop(name)
-
-    local event = require("event")
-
-    event.ignore(events[name].signal, events[name].handler)
-    events[name] = nil
-
-end
-
-function me.dropAll()
-
-    local event = require ("event")
-
-    for key,value in pairs(events) do
-        event.ignore(value["signal"], value["handler"])
+        obj._instance = instance
+        return obj._instance
     end
 
-    events = nil
 
-end
 
-local classa = require ("class/singleton")
-local classb = classa.class.new()
-classb.eC = me
-return classb
+
+    function obj.addEvent(name, signal, handler)
+
+        events[name] = {}
+        events[name].signal = signal
+        events[name].handler = handler
+
+        local eventHandler = require ("eventHandler/"..handler)
+
+        local event = require ("event")
+        event.listen(signal, eventHandler)
+
+
+    end
+
+    function obj.dropEvent(name)
+
+        local event = require("event")
+
+        event.ignore(events[name].signal, events[name].handler)
+        events[name] = nil
+
+    end
+
+    function obj.dropAllEvents()
+
+        local event = require ("event")
+
+            for key,value in pairs(events) do
+                event.ignore(value["signal"], value["handler"])
+            end
+
+        events = nil
+
+    end
+
+return obj
--- robot/eventHandler/tabletmessage.lua
+++ robot/eventHandler/tabletmessage.lua
@@ -1,10 +1,7 @@
-local me = {}
-
-function me.do(_, _, from, port, _, message)
+local function me(_, _, from, port, _, message)
 
 local command = require("command/"..tostring(message))
 command.do()
-
 end
 
 return me
