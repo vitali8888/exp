ae350f60039ac7cbc30774b1767a7bc205780d50
vitali.b <vitali.b.dev@gmail.com>
2018-04-29 02:46:49 +0300

    fix

--- robot/command/up.lua
+++ robot/command/up.lua
@@ -1,6 +1,7 @@
 local function me(message)
     local robot = require("robot")
     local times = tonumber(message[2])
+    if times < 2 then times = 1 end
     for i=1, times do
         robot.up()
     end
