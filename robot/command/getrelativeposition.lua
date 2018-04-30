c17b002609d4b5371f9b68a0834e47ef71bc243d
vitali.b <vitali.b.dev@gmail.com>
2018-04-30 03:25:52 +0300

    stage

--- robot/command/getrelativeposition.lua
+++ robot/command/getrelativeposition.lua
@@ -4,7 +4,7 @@ local component = require ("component")
 local sender = require ("actions/messageSender")
 
 local x, y, z, str = component.navigation.getPosition()
-local message = "x="..tostring(x).." y="..tostring(y).." z="..tostring(z).." data="..tostring(str)
+local message = "X="..tostring(x).." Y="..tostring(y).." Z="..tostring(z).." data="..tostring(str)
 sender(message)
 
 end
