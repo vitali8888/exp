629af30a7bfddbc28c25d22747105606f12e0578
vitali.b <vitali.b.dev@gmail.com>
2018-05-08 02:53:37 +0300

    fix

--- robot/class/internalPositioning.lua
+++ robot/class/internalPositioning.lua
@@ -92,9 +92,10 @@ function class.getDirection(pos) --relative
         if (pos.x == class.posFrom.x and pos.z == class.posFrom.z and class.isEven(layerID) == true) then return "changelayer" end
     end
 
-    if (pos.x == class.posTo.x and class.isEven(ipos.z) == true) or (pos.x == class.posFrom.x and class.isEven(ipos.z) == false) then direction = "zp" end
+
     if (pos.x == class.posFrom.x and class.isEven(ipos.z) == true) then direction = "xp" end
     if (pos.x == class.posTo.x and class.isEven(ipos.z) == false) then direction = "xn" end
+    if (pos.x == class.posTo.x and class.isEven(ipos.z) == true) or (pos.x == class.posFrom.x and class.isEven(ipos.z) == false) then direction = "zp" end
 
     if (class.isEven(layerID)) then direction = class.invertDirection(direction) end
 
