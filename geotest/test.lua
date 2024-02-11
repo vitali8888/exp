049d5872d1cc34044d91873e84bdafaef9b50e4c
= <=>
2024-02-11 19:42:49 +0200

    geotest

--- geotest/test.lua
+++ geotest/test.lua
@@ -2,7 +2,7 @@ local component = require("component")
 
 local geo = component.geolyzer
 
-for key, value in pairs(component.list) do
+for key, value in pairs(component.list()) do
     print ("key: " .. key .. " value: " .. value)
 end
 
