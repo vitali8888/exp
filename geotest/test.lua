469ff53413fe5794e20e76dae1079b242e02a26c
= <=>
2024-02-11 19:41:31 +0200

    geotest

--- geotest/test.lua
+++ geotest/test.lua
@@ -1,1 +1,8 @@
-print ("hello world")
+local component = require("component")
+
+local geo = component.geolyzer
+
+for key, value in pairs(component.list) do
+    print ("key: " .. key .. " value: " .. value)
+end
+
