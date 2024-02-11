5285a1ebd0161bec0789ca4d5424242c3246cda1
= <=>
2024-02-11 20:29:59 +0200

    geotest

--- geotest/test.lua
+++ geotest/test.lua
@@ -1,7 +1,7 @@
 local component = require("component")
 
 local geo = component.geolyzer
-
+--[[
 local address = ''
 
 for key, value in pairs(component.list()) do
@@ -15,15 +15,23 @@ local tableOfMethods = component.methods(address);
 for key, value in pairs(tableOfMethods) do
     print ("key: " .. key)
 end
---]]
+
 
 for key, value in pairs(geo) do
     print (key)
 end
 
-local doc = component.doc(address, "scan")
 
+local doc = component.doc(address, "scan")
 print (doc)
+--]]
+
+local tableOne = geo.scan(0, 0)
+local tableTwo = geo.scan(1, 0)
+
+for key, value in pairs(tableOne) do
+    print("key: " .. key .. " value: " .. value)
+end
 
 
 
